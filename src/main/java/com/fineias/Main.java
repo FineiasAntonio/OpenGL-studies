f05592b3fa153bc71ffb3ea011d4ad209c5ee9a8
Finéias Antônio <138174957+FineiasAntonio@users.noreply.github.com>
2024-06-11 19:39:41 -0300

    pyramid rotation

--- src/main/java/com/fineias/Main.java
+++ src/main/java/com/fineias/Main.java
@@ -3,13 +3,16 @@ package com.fineias;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.awt.GLJPanel;
+import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.Animator;
+import com.jogamp.opengl.util.FPSAnimator;
 
 import javax.swing.*;
+import java.util.random.RandomGenerator;
 
 public class Main {
 
-    private GLCanvas glCanvas;
+
 
     public static void main(String[] args) {
         GLProfile glProfile = GLProfile.get(GLProfile.GL2);
@@ -20,9 +23,12 @@ public class Main {
 
         GLJPanel panel = new GLJPanel(glCapabilities);
         panel.addGLEventListener(new GLEventListener() {
+
+            private float angle = 0f;
+
             @Override
             public void init(GLAutoDrawable glAutoDrawable) {
-                new Thread(() -> {while (true) panel.display();}).start();
+
             }
 
             @Override
@@ -34,20 +40,88 @@ public class Main {
             public void display(GLAutoDrawable glAutoDrawable) {
                 GL2 gl2 = glAutoDrawable.getGL().getGL2();
                 gl2.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+                gl2.glLoadIdentity();
 
+                GLU glu = GLU.createGLU(gl2);
+                glu.gluLookAt(
+                        0.0, 0.0, 5.0,
+                        0.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0
+                );
+
+                gl2.glRotatef(angle, 1.0f, 1.0f, 0.0f);
+
+                // Pyramid faces
                 gl2.glBegin(GL2.GL_TRIANGLES);
-                gl2.glColor3f(1.0f, 0.0f, 0.0f); // Vermelho
-                gl2.glVertex3f(1.0f, 0.0f, 0.0f);
-                gl2.glColor3f(0.0f, 1.0f, 0.0f); // Verde
+
+                /*
+                 * Front triangle
+                 */
+                gl2.glColor3f(1.0f, 0.0f, 0.0f);
                 gl2.glVertex3f(0.0f, 1.0f, 0.0f);
-                gl2.glColor3f(0.0f, 0.0f, 1.0f); // Azul
-                gl2.glVertex3f(-1.0f, 0.0f, 0.0f);
+                gl2.glColor3f(0.0f, 1.0f, 0.0f);
+                gl2.glVertex3f(-1.0f, -1.0f, 1.0f);
+                gl2.glColor3f(0.0f, 0.0f, 1.0f);
+                gl2.glVertex3f(1.0f, -1.0f, 1.0f);
+                /*
+                 * Right triangle
+                 */
+                gl2.glColor3f(1.0f, 0.0f, 0.0f);
+                gl2.glVertex3f(0.0f, 1.0f, 0.0f);
+                gl2.glColor3f(0.0f, 0.0f, 1.0f);
+                gl2.glVertex3f(1.0f, -1.0f, 1.0f);
+                gl2.glColor3f(0.0f, 1.0f, 0.0f);
+                gl2.glVertex3f(1.0f, -1.0f, -1.0f);
+                /*
+                 * Back triangle
+                 */
+                gl2.glColor3f(1.0f, 0.0f, 0.0f);
+                gl2.glVertex3f(0.0f, 1.0f, 0.0f);
+                gl2.glColor3f(0.0f, 1.0f, 0.0f);
+                gl2.glVertex3f(1.0f, -1.0f, -1.0f);
+                gl2.glColor3f(0.0f, 0.0f, 1.0f);
+                gl2.glVertex3f(-1.0f, -1.0f, -1.0f);
+                /*
+                 * Left triangle
+                 */
+                gl2.glColor3f(1.0f, 0.0f, 0.0f);
+                gl2.glVertex3f(0.0f, 1.0f, 0.0f);
+                gl2.glColor3f(0.0f, 0.0f, 1.0f);
+                gl2.glVertex3f(-1.0f, -1.0f, -1.0f);
+                gl2.glColor3f(0.0f, 1.0f, 0.0f);
+                gl2.glVertex3f(-1.0f, -1.0f, 1.0f);
+
                 gl2.glEnd();
+                gl2.glFlush();
+
+                // Pyramid base
+                gl2.glBegin(GL2.GL_QUADS);
+
+                gl2.glVertex3f(-1.0f, -1.0f,1.0f);
+                gl2.glVertex3f(-1.0f, -1.0f,-1.0f);
+                gl2.glVertex3f(1.0f, -1.0f,-1.0f);
+                gl2.glVertex3f(1.0f, -1.0f,1.0f);
+
+                gl2.glEnd();
+
+                gl2.glFlush();
+
+                angle += 0.3f;
+
             }
+
+
 
             @Override
             public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
-
+                GL2 gl2 = glAutoDrawable.getGL().getGL2();
+                gl2.glViewport(0, 0, i2, i3);
+                gl2.glMatrixMode(GL2.GL_PROJECTION);
+                gl2.glLoadIdentity();
+                GLU glu = GLU.createGLU(gl2);
+                glu.gluPerspective(45.0, (double) i2 / i3, 1.0, 100.0);
+                gl2.glMatrixMode(GL2.GL_MODELVIEW);
+                gl2.glLoadIdentity();
             }
         });
         panel.setSize(400,400);
@@ -58,8 +132,8 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
-        
-        Animator animator = new Animator(panel);
+
+        final FPSAnimator animator = new FPSAnimator(panel, 400,true );
         animator.start();
     }
 
